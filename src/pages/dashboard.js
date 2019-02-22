5f4e016fd4254a8920afa6db7fb6e15b887b9464
Bryan McConnahea <transcendrstudio@gmail.com>
2019-02-22 19:52:33 +0100

    Completes UI controls

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types'
 import Layout from '../components/layout'
 import ProgressBar from '../components/progress-bar'
 import FacebookLogin from 'react-facebook-login'
-
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Alert from 'react-bootstrap/Alert'
 import '../../node_modules/bootstrap/dist/css/bootstrap.css'
 
 class DashboardPage extends Component {
@@ -14,11 +18,19 @@ class DashboardPage extends Component {
       authData: {},
       appId: null,
       isUser: false,
+      adAccounts: [],
+      settings: {},
+      formValid: true,
     }
     this.debug = true
     this.apiBaseURL = 'https://jn82y8mrx5.sse.codesandbox.io'
     this.apiAuthToken = ''
     this.apiRequestKey = 'client'
+    this.settings = {
+      apiKey: '',
+      adAccountId: '',
+      buildQueue: 0,
+    }
   }
 
   componentWillMount() {
@@ -82,8 +94,9 @@ class DashboardPage extends Component {
       .split(',')
     try {
       parsed = JSON.parse(parsed)
-      const { fbAccountId, apiKey } = parsed
+      const { adAccountId, fbAccountId, apiKey } = parsed
       result = {
+        adAccountId,
         fbAccountId,
         apiKey,
       }
@@ -93,7 +106,6 @@ class DashboardPage extends Component {
         apiKey: parsed[1],
       }
     }
-    console.warn('PARSED API', result)
     return result
   }
 
@@ -106,12 +118,14 @@ class DashboardPage extends Component {
 
   checkUser = async () => {
     const { id } = this.state.authData
-    const query = `mutation{isUser(fbAccountId: "${id}") {exists isReady token}}`
+    const query = `mutation{isUser(fbAccountId: "${id}") {exists ready token settings{adAccountId}}}`
     // Check user status by FB account ID
     const response = await this.apiRequest(this.apiRequestKey, query)
     this.handleApiError(response, true, 'checkUser')
     // Set user status to state
     this.setState({ isUser: response.data.isUser })
+    // Save retrieved user settings
+    this.setState({ settings: response.data.isUser.settings })
     // Save the single-use client auth token
     this.apiAuthToken = response.data.isUser.token
     // Parse and set request key
@@ -124,7 +138,8 @@ class DashboardPage extends Component {
   loginUser = async () => {
     let response
     const { isUser, authData } = this.state
-    const { exists } = isUser
+    const { exists, ready } = isUser
+    let settings = this.state.settings
     let query
     if (!exists) {
       // login new user with FB access token
@@ -137,32 +152,72 @@ class DashboardPage extends Component {
       response = await this.apiRequest(this.apiRequestKey, query)
       this.handleApiError(response, true, 'loginUser (exists)')
     }
+
     // Save the new user auth token
     this.apiAuthToken = response.data.authToken
+
+    // Setup settings
+    const { apiKey } = this.parseAuthToken()
+    settings.apiKey = apiKey
+    settings.accessToken = authData.accessToken
+    this.setState({ settings })
+
+    // Update user
+    this.updateUser(false)
+
+    // Set user ad accounts to state
+    this.setUserAdAccounts()
   }
 
-  updateUser = async () => {
-    console.log('AuthData', {
-      authToken: this.apiAuthToken,
-      parsedAuthToken: this.parseAuthToken(),
-    })
-    const variables = {
-      adAccountId: '1804348333164926',
-      apiKey: '123456',
-      buildQueue: 1,
-      accessToken: this.state.authData.accessToken,
-    }
-    const query = `mutation{updateUser(adAccountId: "${
-      variables.adAccountId
-    }", apiKey: "${variables.apiKey}", buildQueue: ${
-      variables.buildQueue
-    }, accessToken: "${variables.accessToken}") {success authToken}}`
-    // Request user data update
+  setUserAdAccounts = async () => {
+    let { settings } = this.state
+    // Request user ad account ID's
+    const query = `query {adaccounts{results{id}}}`
     const response = await this.apiRequest(this.apiRequestKey, query, true)
-    this.handleApiError(response, true, 'updateUser')
-    // Upon success save the permanent user-ready auth token
-    this.apiAuthToken = response.data.updateUser.authToken
-    this.apiRequestKey = variables.apiKey
+    this.handleApiError(response, true, 'setUserAdAccounts')
+    const adAccounts = response.data.adaccounts.results
+    // Set results to state
+    this.setState({ adAccounts })
+    // If user doesn't have saved ad account ID, set it to the first result
+    settings.adAccountId = settings.adAccountId
+      ? settings.adAccountId
+      : adAccounts[0].id
+    this.setState({ settings })
+  }
+
+  updateUser = async showMessage => {
+    const { settings } = this.state
+    this.apiRequestKey = settings.apiKey ? settings.apiKey : this.apiRequestKey
+    // Setup iterable valid form keys
+    const keys = ['adAccountId', 'apiKey', 'buildQueue', 'accessToken']
+    // Iterate form keys to build request query
+    let query = 'mutation{updateUser('
+    for (const key of keys) {
+      query += settings[key] ? `${key}: "${settings[key]}",` : ''
+    }
+    query = query.slice(0, -1)
+    query += `) {success authToken isUser{ready}}}`
+    // Request user update
+    const response = await this.apiRequest(this.apiRequestKey, query, true)
+    this.handleApiError(response, false, 'updateUser')
+    // Check for error saving
+    if (response.errors) {
+      alert(
+        'There was an issue saving.  Please try again, verifying that you are saving a valid API key.'
+      )
+    } else {
+      // Upon success, show message
+      if (showMessage !== false) {
+        const success = { saveSuccess: 'Settings saved successfuly.' }
+        this.setState({ formValid: success })
+        setTimeout(() => this.setState({ formValid: {} }), 3000)
+      }
+      // save the permanent user-ready auth token
+      this.apiAuthToken = response.data.updateUser.authToken
+      this.apiRequestKey = settings.apiKey
+        ? settings.apiKey
+        : this.apiRequestKey
+    }
   }
 
   responseFacebook = response => {
@@ -172,27 +227,86 @@ class DashboardPage extends Component {
     this.checkUser(response.id)
   }
 
-  componentClicked(e) {
-    console.log(e)
+  handleFormChange = e => {
+    const { target } = e
+    const key = target.id.split('.')[1]
+    let { settings } = this.state
+    settings[key] = target.value
+    this.setState({ settings })
+    this.validateForm()
+  }
+
+  validateForm() {
+    let formValid = {}
+    const { settings } = this.state
+    if (!settings.apiKey || !settings.apiKey.trim()) {
+      formValid.error = 'API Key cannot be empty'
+    }
+    this.setState({ formValid })
+    return formValid
   }
 
   DashboardLayout = () => {
-    const { isUser } = this.state
+    const { adAccounts, settings, formValid } = this.state
+    const { apiKey } = this.parseAuthToken()
+
     return (
       <Layout>
         <h1> Dashboard </h1>
-        <p> This is where the magic shall happen </p>
-        <ul>
-          <li>isUser: {isUser.exists ? 'Yes' : 'No'}</li>
-          <li>isReady: {isUser.isReady ? 'Yes' : 'No'}</li>
-        </ul>
-        <button
-          type="button"
-          onClick={() => this.updateUser()}
-          className="btn btn-primary"
-        >
-          Primary
-        </button>
+
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group controlId="settings.apiKey">
+                <Form.Label>API Key</Form.Label>
+                <Form.Control
+                  defaultValue={apiKey}
+                  onChange={e => this.handleFormChange(e)}
+                  type="text"
+                  placeholder="Enter a API key"
+                />
+              </Form.Group>
+              <Form.Group controlId="settings.adAccountId">
+                <Form.Label>Ad Account</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={settings.adAccountId || ''}
+                  onChange={e => this.handleFormChange(e)}
+                >
+                  {adAccounts.map((x, index) => (
+                    <option value={x.id} key={index}>
+                      {x.id}
+                    </option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Alert
+              defaultShow={false}
+              show={!!formValid.error}
+              variant="danger"
+            >
+              {formValid.error}
+            </Alert>
+            <Alert
+              defaultShow={false}
+              show={!!formValid.saveSuccess}
+              variant="success"
+            >
+              {formValid.saveSuccess}
+            </Alert>
+            <Button
+              disabled={!!formValid.error ? 'disabled' : ''}
+              onClick={() => this.updateUser()}
+            >
+              Save
+            </Button>
+          </Col>
+        </Row>
       </Layout>
     )
   }
