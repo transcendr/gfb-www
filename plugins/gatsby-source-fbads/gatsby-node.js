772579d59331db9acce95713aed32bdb977a5f8d
Bryan McConnahea <transcendrstudio@gmail.com>
2019-02-23 20:16:07 +0100

    Fixes build query

--- plugins/gatsby-source-fbads/gatsby-node.js
+++ plugins/gatsby-source-fbads/gatsby-node.js
@@ -61,7 +61,7 @@ exports.sourceNodes = async (
 
   const fetchAds = async after => {
     const body = {
-      query: `query {adcreatives(limit: ${limit}, after: ${after}) {results {id,title,body,image_url},hasMore,after}}`,
+      query: `query {adcreatives(limit: 25, after: "${after}") {results {id,title,body,image_url},hasMore,after}}`,
     }
     const rawResponse = await fetch(apiUrl, {
       method: 'POST',
